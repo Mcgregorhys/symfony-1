982d0833917533ce60165689241b6c632db12329
Mcgregorhys <ghys@wp.pl>
2025-04-09 09:33:55 +0200

    koniec lekcji 53 rejestracja użytkownika

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,7 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
