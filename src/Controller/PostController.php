7b384c44e54fc7a0c31f0ad5520eaad76f732f63
Mcgregorhys <ghys@wp.pl>
2025-04-12 09:41:48 +0200

    koniec lekcji 59

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,29 +5,44 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-
+use App\Entity\Post;
+use App\Form\PostType;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/', requirements: ['_locale' => 'en|pl'])]
-
 class PostController extends AbstractController
 {
     #[Route('/{_locale}', methods: ['GET'], name: 'posts.index')]
     public function index(string $_locale = 'en'): Response
     {
-       
-       
         return $this->render('post/index.html.twig');
     }
 
-    #[Route('/{_locale}/post/new/', methods: ['GET', 'POST'], name: 'posts.new')]
-    public function new(): Response
+    #[Route('/{_locale}/post/new', methods: ['GET', 'POST'], name: 'posts.new')]
+    public function new(Request $request): Response
     {
-        
-        return $this->render('post/new.html.twig');
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $post = new Post();
+        // $post->setTitle('Write a blog post');
+        // $post->setContent('Post content');
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $post = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+
+            return $this->redirectToRoute('posts.index');
+        }
+        return $this->render('post/new.html.twig', [
+            'form' => $form,
+        ]);
     }
 
-    #[Route('/{_locale}/post/{id}/', methods: ['GET'], name: 'posts.show')]
+    #[Route('/{_locale}/post/{id}', methods: ['GET'], name: 'posts.show')]
     public function show($id): Response
     {
         return $this->render('post/show.html.twig');
@@ -37,12 +52,14 @@ class PostController extends AbstractController
     public function edit($id): Response
     {
         // return $this->redirectToRoute('posts.index');
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         return $this->render('post/edit.html.twig');
     }
 
     #[Route('/{_locale}/post/{id}/delete', methods: ['POST'], name: 'posts.delete')]
     public function delete($id): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         return new Response('delete post from the database');
     }
 
@@ -57,6 +74,7 @@ class PostController extends AbstractController
     public function toggleFollow($user): Response
     {
         // return new Response($user);
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         return new Response('logic for toggling like/dislike functionality');
     }
 }
